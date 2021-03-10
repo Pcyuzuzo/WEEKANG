fdf7adab21e6562f32487373e816e29f98526082
patience <PCyuzuzo320@daviscollege.com>
2021-03-10 05:18:45 -0500

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+
+import { NgModule } from '@angular/core';
+
+//import the http NgModule
+
+
+//adding the ngmodel issue
+import { FormsModule } from '@angular/forms';
+
+//import the search SearchService with the code below
+import { SearchService } from './services/search.service';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpModule,
+    FormsModule
+  ],
+  providers: [SearchService],//copied SearchService here to make it avalable throughout the app
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
